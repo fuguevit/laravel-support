bddbfbbef580c7e90f6556d172ca49df4a494977
wangxuan <wangxuan@qgpic.com>
2016-11-11 19:19:53 +0800

    init supportserviceprovider

--- src/Providers/SupportServiceProvider.php
+++ src/Providers/SupportServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace Fuguevit\Support;
 
+use Fuguevit\Support\Console\Commands\Creators\HelperCreator;
+use Fuguevit\Support\Console\Commands\MakeHelperCommand;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
 use Illuminate\Support\ServiceProvider;
 
 class SupportServiceProvider extends ServiceProvider
@@ -11,7 +15,10 @@ class SupportServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        $this->publishes([
+            __DIR__.'/../../config/error-code.php' => config_path('error-code.php'),
+            __DIR__.'/../../config/support.php'    => config_path('support.php')
+        ], 'config');
     }
 
     /**
@@ -19,7 +26,55 @@ class SupportServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Register binds.
+        $this->registerBinds();
+        // Register make:helper command.
+        $this->registerMakeHelperCommands();
+        // Register commands.
+        $this->commands(['command.helper.make']);
         
+        $config_support_path = __DIR__.'/../../config/support.php';
+        // Merge config.
+        $this->mergeConfigFrom($config_support_path, 'support');
+    }
+
+    /**
+     * Register the bindings.
+     */
+    protected function registerBindings()
+    {
+        // Filesystem
+        $this->app->instance('FileSystem', new Filesystem());
+        // Composer
+        $this->app->bind('Composer', function($app) {
+            return new Composer($app['FileSystem']);
+        });
+        // HelperCreator
+        $this->app->singleton('HelperCreator', function($app) {
+           return new HelperCreator($app['FileSystem']); 
+        });
+    }
+
+    /**
+     * Register the make:helper command.
+     */
+    protected function registerMakeHelperCommands()
+    {
+        $this->app['command.helper.make'] = $this->app->share([
+            function($app) {
+                return new MakeHelperCommand($app['HelperCreator']);
+            }
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function provides()
+    {
+        return [
+            'command.helper.make'
+        ];
     }
 
 }
